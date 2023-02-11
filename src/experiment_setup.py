aa5ad24470c7105e5d28932e0a8cfee0b6627acc
David He <davidhe137@gmail.com>
2023-02-11 11:47:44 -0500

    Add experiment_setup.py and config.sh

---
+++ src/experiment_setup.py
@@ -0,0 +1,112 @@
+import os
+from pathlib import Path
+import argparse
+from utils import *
+
+import random
+from datetime import datetime
+from copy import deepcopy
+from shutil import copyfile
+
+
+def main():      
+    '''
+    Either pass reference experiment ID or train/test splits
+    '''
+    parser = argparse.ArgumentParser(description='Configure dataset, model-size, method, data splits.')
+    parser.add_argument('--dataset', choices=['BoolQ'])
+    parser.add_argument('--model_size', choices=['350m'])
+    parser.add_argument('--method', choices=['mice_sampling'])
+    parser.add_argument('--in_context', type=int)
+
+    parser.add_argument('--reference_experiment_id', default=0, type=int) # only positive experiment IDs
+
+    parser.add_argument('--train', type=int)
+    parser.add_argument('--test', type=int)
+
+    args = parser.parse_args()
+
+    # gather absolute paths
+    project_root = Path(__file__).resolve().parents[1]
+    data_home = os.path.join(project_root, 'data')
+    exp_home = os.path.join(project_root, 'experiments')
+
+    # gather details
+    exp_summary = os.path.join(exp_home, 'summary.json') 
+    exp_id = 1
+
+    exp_summary_data = {'summary': []}
+    if os.path.exists(exp_summary):        
+        exp_summary_data = read_json(exp_summary)
+        past_exp_ids = {exp['id'] for exp in exp_summary_data['summary']}
+        exp_id = max(past_exp_ids) + 1        
+
+    exp_dir = os.path.join(exp_home, args.dataset, args.model_size, args.method, 
+                            f"train{args.train}_test{args.test}_id{exp_id}")
+    summary = {
+        'id': exp_id,
+        'created': str(datetime.now()),
+        'location': exp_dir,
+        'dataset': args.dataset,
+        'model_size': args.model_size,
+        'method': args.method,
+        'in_context': args.in_context,
+        'train': args.train,
+        'test': args.test
+    }
+    info = deepcopy(summary)
+
+    # choose same examples as --reference_experiment
+    if args.reference_experiment_id > 0:
+        # find reference experiment data
+        exp_summary_data = read_json(exp_summary)
+        ref_exp = list(filter(lambda x: x['id'] == args.reference_experiment_id, exp_summary_data['summary']))
+        assert len(ref_exp) == 1
+        ref_exp = ref_exp[0]
+
+        summary['train'] = ref_exp['train']
+        summary['test'] = ref_exp['test']
+        info['train'] = summary['train']
+        info['test'] = summary['test']
+
+        exp_dir = os.path.join(exp_home, args.dataset, args.model_size, args.method, 
+                f"train{summary['train']}_test{summary['test']}_id{exp_id}") 
+        os.makedirs(exp_dir, exist_ok=True)
+
+        ref_exp_dir = ref_exp['location']
+        copyfile(os.path.join(ref_exp_dir, 'train.jsonl'), os.path.join(exp_dir, 'train.jsonl'))
+        copyfile(os.path.join(ref_exp_dir, 'test.jsonl'), os.path.join(exp_dir, 'test.jsonl'))
+        copyfile(os.path.join(ref_exp_dir, 'test_ids.txt'), os.path.join(exp_dir, 'test_ids.txt'))
+
+
+        ref_exp_info = read_json(os.path.join(ref_exp_dir, 'info.json'))
+        info['train_ids'] = ref_exp_info['train_ids']
+        info['test_ids'] = ref_exp_info['test_ids']
+        
+
+    else:
+        data_dir = os.path.join(data_home, args.dataset)
+        train = read_jsonl(os.path.join(data_dir, 'train.jsonl'))
+        test = read_jsonl(os.path.join(data_dir, 'val.jsonl')) # original datasets only have labels for validation
+
+        # sample k demonstrations for n test examples
+        train = random.choices(train, k=args.train)
+        test = random.choices(test, k=args.test)
+        train.sort(key=lambda x: x['idx'])
+        test.sort(key=lambda x: x['idx'])
+
+        os.makedirs(exp_dir, exist_ok=True)
+        write_jsonl(train, os.path.join(exp_dir, 'train.jsonl'))
+        write_jsonl(test, os.path.join(exp_dir, 'test.jsonl'))
+        write_txt([str(example['idx']) for example in test], os.path.join(exp_dir, 'test_ids.txt'))
+
+        info['train_ids'] = [example['idx'] for example in train]
+        info['test_ids'] = [example['idx'] for example in test]
+    
+
+    exp_summary_data['summary'].append(summary)
+    write_json(exp_summary_data, exp_summary)
+    write_json(info, os.path.join(exp_dir, 'info.json'))
+        
+if __name__ == '__main__':
+    main()
---
+++ src/utils.py
@@ -0,0 +1,44 @@
+import json
+
+def read_jsonl(filepath: str) -> dict:    
+    data = list()
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            example = json.loads(line)
+            data.append(example)
+    return data
+
+
+def write_jsonl(data: list, filepath: str) -> None:
+    with open(filepath, "w") as f:
+        for example in data:
+            f.write(json.dumps(example) + "\n")
+
+def append_jsonl(data: list, filepath: str) -> None:
+    with open(filepath, "a") as f:
+        for example in data:
+            f.write(json.dumps(example) + "\n")
+
+
+def read_txt(filepath: str) -> list:
+    data = []
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            data.append(line.strip())
+    return data
+
+
+def write_txt(data: list, filepath: str) -> None:
+    with open(filepath, "w") as f:
+        for line in data:
+            f.write(line + "\n")
+
+
+def read_json(filepath: str) -> dict:
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def write_json(d: dict, filepath: str) -> None:
+    with open(filepath, "w") as f:
+        json.dump(d, f, indent=4)
