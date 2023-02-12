18a6d5d446a2abd3ce6e841696c4af6d62ddd831
David He <davidhe137@gmail.com>
2023-02-12 13:48:46 -0500

    Fix dataset logging for experiment setup.

--- src/experiment_setup.py
+++ src/experiment_setup.py
@@ -8,21 +8,22 @@ from datetime import datetime
 from copy import deepcopy
 from shutil import copyfile
 
-
 def main():      
     '''
-    Either pass reference experiment ID or train/test splits
+    Create experiment folder, set up train/test splits, log details
     '''
     parser = argparse.ArgumentParser(description='Configure dataset, model-size, method, data splits.')
-    parser.add_argument('--dataset', choices=['BoolQ'])
     parser.add_argument('--model_size', choices=['350m'])
     parser.add_argument('--method', choices=['mice_sampling'])
-    parser.add_argument('--in_context', type=int)
+    parser.add_argument('--in_context', default='1', type=int)
+    parser.add_argument('--num_prompts', type=int)
+
+    parser.add_argument('--dataset', choices=['BoolQ'])
+    parser.add_argument('--train', type=int)
+    parser.add_argument('--test', type=int)
 
     parser.add_argument('--reference_experiment_id', default=0, type=int) # only positive experiment IDs
 
-    parser.add_argument('--train', type=int)
-    parser.add_argument('--test', type=int)
 
     args = parser.parse_args()
 
@@ -41,21 +42,6 @@ def main():
         past_exp_ids = {exp['id'] for exp in exp_summary_data['summary']}
         exp_id = max(past_exp_ids) + 1        
 
-    exp_dir = os.path.join(exp_home, args.dataset, args.model_size, args.method, 
-                            f"train{args.train}_test{args.test}_id{exp_id}")
-    summary = {
-        'id': exp_id,
-        'created': str(datetime.now()),
-        'location': exp_dir,
-        'dataset': args.dataset,
-        'model_size': args.model_size,
-        'method': args.method,
-        'in_context': args.in_context,
-        'train': args.train,
-        'test': args.test
-    }
-    info = deepcopy(summary)
-
     # choose same examples as --reference_experiment
     if args.reference_experiment_id > 0:
         # find reference experiment data
@@ -64,25 +50,31 @@ def main():
         assert len(ref_exp) == 1
         ref_exp = ref_exp[0]
 
-        summary['train'] = ref_exp['train']
-        summary['test'] = ref_exp['test']
-        info['train'] = summary['train']
-        info['test'] = summary['test']
-
         exp_dir = os.path.join(exp_home, args.dataset, args.model_size, args.method, 
-                f"train{summary['train']}_test{summary['test']}_id{exp_id}") 
+                f"train{ref_exp['train']}_test{ref_exp['test']}_id{exp_id}") 
         os.makedirs(exp_dir, exist_ok=True)
 
+        summary = {
+            'id': exp_id,
+            'created': str(datetime.now()),
+            'location': exp_dir,
+            'dataset': ref_exp['dataset'],
+            'model_size': args.model_size,
+            'method': args.method,
+            'in_context': args.in_context,
+            'train': ref_exp['train'],
+            'test': ref_exp['test']
+        }
+        info = deepcopy(summary)
+
         ref_exp_dir = ref_exp['location']
-        copyfile(os.path.join(ref_exp_dir, 'train.jsonl'), os.path.join(exp_dir, 'train.jsonl'))
-        copyfile(os.path.join(ref_exp_dir, 'test.jsonl'), os.path.join(exp_dir, 'test.jsonl'))
-        copyfile(os.path.join(ref_exp_dir, 'test_ids.txt'), os.path.join(exp_dir, 'test_ids.txt'))
-
-
         ref_exp_info = read_json(os.path.join(ref_exp_dir, 'info.json'))
         info['train_ids'] = ref_exp_info['train_ids']
         info['test_ids'] = ref_exp_info['test_ids']
-        
+
+        copyfile(os.path.join(ref_exp_dir, 'train.jsonl'), os.path.join(exp_dir, 'train.jsonl'))
+        copyfile(os.path.join(ref_exp_dir, 'test.jsonl'), os.path.join(exp_dir, 'test.jsonl'))
+        copyfile(os.path.join(ref_exp_dir, 'test_ids.txt'), os.path.join(exp_dir, 'test_ids.txt'))
 
     else:
         data_dir = os.path.join(data_home, args.dataset)
@@ -91,18 +83,36 @@ def main():
 
         # sample k demonstrations for n test examples
         train = random.choices(train, k=args.train)
-        test = random.choices(test, k=args.test)
+        if args.test > 0: # pass -1 to test all examples
+            test = random.choices(test, k=args.test)
+        else:
+            args.test = len(test)
         train.sort(key=lambda x: x['idx'])
         test.sort(key=lambda x: x['idx'])
 
+        exp_dir = os.path.join(exp_home, args.dataset, args.model_size, args.method, 
+                        f"train{args.train}_test{args.test}_id{exp_id}")
         os.makedirs(exp_dir, exist_ok=True)
-        write_jsonl(train, os.path.join(exp_dir, 'train.jsonl'))
-        write_jsonl(test, os.path.join(exp_dir, 'test.jsonl'))
-        write_txt([str(example['idx']) for example in test], os.path.join(exp_dir, 'test_ids.txt'))
+
+        summary = {
+            'id': exp_id,
+            'created': str(datetime.now()),
+            'location': exp_dir,
+            'dataset': args.dataset,
+            'model_size': args.model_size,
+            'method': args.method,
+            'in_context': args.in_context,
+            'train': args.train,
+            'test': args.test
+        }
+        info = deepcopy(summary)
 
         info['train_ids'] = [example['idx'] for example in train]
         info['test_ids'] = [example['idx'] for example in test]
-    
+
+        write_jsonl(train, os.path.join(exp_dir, 'train.jsonl'))
+        write_jsonl(test, os.path.join(exp_dir, 'test.jsonl'))
+        write_txt([str(example['idx']) for example in test], os.path.join(exp_dir, 'test_ids.txt'))
 
     exp_summary_data['summary'].append(summary)
     write_json(exp_summary_data, exp_summary)
